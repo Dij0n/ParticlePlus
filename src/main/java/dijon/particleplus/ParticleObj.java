ab4fe9459865101a00ee294f0ee0b44fe9a2c807
Dij0n <dijonmustrdlive@gmail.com>
2024-04-18 21:39:24 +0300

    Initial Commit

---
+++ src/main/java/dijon/particleplus/ParticleObj.java
@@ -0,0 +1,21 @@
+package dijon.particleplus;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class ParticleObj {
+
+    Location loc;
+    Player owner;
+    double speed;
+    int count;
+
+    public ParticleObj(Location loc, Player owner, double speed, int count) {
+        this.loc = loc;
+        this.owner = owner;
+        this.speed = speed;
+        this.count = count;
+    }
+
+
+}
